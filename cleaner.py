4885e1a555d02dd69bf8f672a2d42675f074939f
inv0w <jonjoninfante@gmail.com>
2020-03-23 22:33:52 -0700

    Stopper and Stemmer changes

--- cleaner.py
+++ cleaner.py
@@ -11,7 +11,7 @@ words = stopwords.words('english')
 # The process of breaking a word down to its root, prefixes and suffixes
 stemmer = SnowballStemmer('english')
 
-#Cleaning up article text to only alpha characters
+#Cleaning up article text to only alpha characters as well as lowercase
 df['cleaned'] = df['content'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
 df2['cleaned'] = df2['content'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
 
