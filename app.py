cd90924bbecf45650eaf8cf4683da9dffa8eb616
inv0w <jonjoninfante@gmail.com>
2020-03-23 22:30:52 -0700

    Reversed stemmer and stopper words

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ def index():
 @app.route('/detector', methods=['GET', 'POST'])
 def detector():
     """Return bias detector page."""
-
+    # Tried to speed up the process on Heruko specifcally with AWS.
+    # Turned out to be slower
+    # s3 = boto3.resource('s3',
+    #  aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    #  aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    #
+    # bias_model = BytesIO(s3.Bucket("bias-detector").Object("bias_model.pkl").get()['Body'].read())
+    # with open(bias_model, 'rb') as file:
+    #     content = pickle.load(file)
     if 'bias_text' in request.form:
         with open('bias_model.pkl', 'rb') as file:
             bias_model = joblib.load(file)
-        # Tried to speed up the process on Heruko specifcally with AWS.
-        # Turned out to be slower
-        # s3 = boto3.resource('s3',
-        #  aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        #  aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-        #
-        # bias_model = BytesIO(s3.Bucket("bias-detector").Object("bias_model.pkl").get()['Body'].read())
-        # with open(bias_model, 'rb') as file:
-        #     content = pickle.load(file)
+
         bias_text = request.form['bias_text']
         textbox = request.form['bias_text']
         bias = bias_model.predict([bias_text])
     else:
-        bias = ' '
+        bias = ''
         textbox = 'Your text here'
 
     return render_template('detector.html', bias_prediction=bias, textbox=textbox)
--- cleaner.py
+++ cleaner.py
@@ -7,8 +7,8 @@ from nltk.stem import SnowballStemmer
 df = pd.read_csv('env/datasets/articles1.csv')
 df2 = pd.read_csv('env/datasets/articles2.csv')
 
-stemmer = SnowballStemmer('english')
 words = stopwords.words("english")
+stemmer = SnowballStemmer('english')
 
 #Cleaning up article text to only alpha characters
 df['cleaned'] = df['content'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
